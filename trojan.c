8a790f3a7d28991c69332c064ecefc6c305f35f7
thejasp <salinaj2@miamoh.edu>
2023-04-03 00:14:26 -0400

    Added code cave struct

--- trojan.c
+++ trojan.c
@@ -3,7 +3,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-int get_code_cave(int fd) {
+struct code_cave {
+	uint64_t cave_location;
+	uint64_t cave_size;
+};
+
+void get_code_cave(int fd, struct code_cave *cc) {
 	char buf[1024];
 	int max_cave_location = 0;
 	int max_cave_size = 0;
@@ -24,7 +29,6 @@ int get_code_cave(int fd) {
 		for (int i = 0; i < bytes_read; ++i) {
 			cur_hex = buf[i];
 			if ( prev_hex != 0  && cur_hex == 0) {
-				printf("New Cave 0x%x\n", prev_offset + i);
 				cave_start =  i + prev_offset;
 			}
 			else if ( cur_hex == 0) {
@@ -46,7 +50,11 @@ int get_code_cave(int fd) {
 
 	printf("Largest Cave size: %d\n", max_cave_size);
 	printf("Largest Cave location: 0x%x\n", max_cave_location);
-	return max_cave_location;
+
+	cc->cave_size = max_cave_size;
+	cc->cave_location = max_cave_location;
+
+//	return max_cave_location;
 }
 
 int main(int argc, char **argv) {
@@ -56,8 +64,8 @@ int main(int argc, char **argv) {
 	int fd = open("hello", O_RDWR);
 
 	read(fd, &ehdr, sizeof(ehdr));
-
-	get_code_cave(fd);
+	struct code_cave cc;
+	get_code_cave(fd, &cc);
 	//printf("Entrypoint: 0%x\n", ehdr.e_entry);
 	//ehdr.e_entry = 0x1111111;
 	//lseek(fd, 0, SEEK_SET);
@@ -69,10 +77,11 @@ int main(int argc, char **argv) {
 	//for (int i = 0; i < 15; ++i) {
 	//	read(fd, &shdr, sizeof(shdr));
 	//}
-	
-	printf("Section Header size: 0x%x\n", shdr.sh_size);
+	printf("Section Header size: %d\n", shdr.sh_size);
+	printf("Code Cave size: %d\n", cc.cave_size);
 
-	shdr.sh_size = 0xd + 40;
+	shdr.sh_size = 0xd + 1000;
+	printf("New Section Header size: %d\n", shdr.sh_size);
 
 	close(fd);
 	
